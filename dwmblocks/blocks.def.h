d4fecd9b49badb787ddd048dc9b0ae0aca635e11
Scherer <vicentin.py@gmail.com>
2021-10-10 13:20:28 -0300

    Update

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,7 +1,6 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",		"holoen",			600,			6},
 	{"",		"weather",			3600,			5},
 	{"",		"hardware",			60,			4},
 	{"",		"network",			1,			3},
