7e21d428fee1162b68f7c7ead9810c5cd5e66065
Scherer <vicentin.py@gmail.com>
2021-11-20 00:11:07 -0300

    Add dynamic cursor color patch

--- st/config.def.h
+++ st/config.def.h
@@ -8,6 +8,9 @@
 static char *font = "Source Code Pro:pixelsize=18:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
+	"Braille:pixelsize=10:antialias=true:autohint=true",
+	"JoyPixels:pixelsize=10:antialias=true:autohint=true",
+	"Noto Color Emoji:pixelsize=11:antialias=true:autohint=true",
 	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true",
 	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true",
 };
@@ -100,7 +103,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.9;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
