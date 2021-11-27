7ee4ce7b32aae32447b3271a0ae55ec1c6c95a1f
Scherer <vicentin.py@gmail.com>
2021-11-27 15:49:04 -0300

    Add tag preview

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -9,6 +9,7 @@ static const unsigned int gappoh    = 5;       /* horiz outer gap between window
 static const unsigned int gappov    = 5;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
+static const int scalepreview       = 4;        /* tag preview scaling */
 static const int topbar             = 1;        /* 0 means bottom bar */
 #define ICONSIZE 16
 #define ICONSPACING 5
