5abaf499537c4422c50471951b362d05dd9ea90e
Scherer <vicentin.py@gmail.com>
2021-11-27 20:12:09 -0300

    Add pertag patch

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -7,7 +7,7 @@ static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappoh    = 6;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 12;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -151,7 +151,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_m,      			spawn,          SHCMD("dmenuselector math") },
 	{ MODKEY|ShiftMask,             XK_p,      			spawn,          SHCMD("simple-scan") },
 	{ MODKEY|ShiftMask,             XK_b,      			spawn,          SHCMD("xdg-open https://searx.be") },
-	{ MODKEY|ShiftMask,             XK_l,      			spawn,          SHCMD("latex2img") },
+	{ MODKEY|ShiftMask,             XK_t,      			spawn,          SHCMD("latex2img") },
 	{ MODKEY|ShiftMask,             XK_equal,  			spawn,          SHCMD("pamixer -i 1 && pkill -RTMIN+2 dwmblocks") },
 	{ MODKEY|ShiftMask,             XK_minus,  			spawn,          SHCMD("pamixer -d 1 && pkill -RTMIN+2 dwmblocks") },
 //	{ MODKEY|ShiftMask,             XK_a,      seticonset,     {.i = 0 } },
